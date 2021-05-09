09f10ea5c94dcf563182eddd6faca28ecff24c3b
Savion Levi <savion.levi@mail.huji.ac.il>
2021-05-09 11:45:06 +0300

    warrior or wizard, what is your pick?

---
+++ Games/Might and Magic/classes/game.py
@@ -0,0 +1,78 @@
+import random
+from pprint import pprint
+
+
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+class Person:
+    #attack, defense
+    #attack high
+    #attack low
+
+    def __init__(self, hp, mp, atk, df, magic):
+        self.maxhp = hp
+        self.hp = hp
+        self.maxmp = mp
+        self.mp = mp
+        self.atkl = atk - 10
+        self.atkh = atk + 10
+        self.df = df
+        self.magic = magic
+        self.actions = ["Attack", "Magic"]
+
+    def generate_damage(self):
+        return random.randrange(self.atkl, self.atkh)
+
+    def take_damange(self, dmg):
+        self.hp -= dmg
+        if self.hp < 0:
+            self.hp = 0
+        return self.hp
+
+    def heal(self, dmg):
+        self.hp += dmg
+        if self.hp > self.maxhp:
+            self.hp = self.maxhp
+
+    def get_hp(self):
+        return self.hp
+
+    def get_max_hp(self):
+        return self.maxhp
+
+    def get_mp(self):
+        return self.mp
+
+    def get_max_mp(self):
+        return self.maxmp
+
+    def reduce_mp(self, cost):
+        self.mp -= cost
+
+
+    def choose_action(self):
+        i = 1
+        print(bcolors.OKBLUE + bcolors.BOLD+ "Actions" + bcolors.ENDC)
+        for item in self.actions:
+            print(str(i) + ":", item)
+            i += 1
+
+    def choose_magic(self):
+        i = 1
+
+        print("Magic")
+        for spell in self.magic:
+            print(str(i) + ":", spell.name, "(cost:", str(spell.cost) + ")")
+            i += 1
+
+
