11fc5633aa3f8fbdbc6295b784e7f046ae304007
Savion Levi <savion.levi@mail.huji.ac.il>
2021-07-07 10:57:47 +0300

    Update mac_memory_test_client.py

--- Performance/mac_memory_test_client.py
+++ Performance/mac_memory_test_client.py
@@ -6,7 +6,7 @@ from datetime import datetime
 def memory_test(iterations):
     iterations = iterations * 1000000
     for i in range(iterations):
-        command = "ps x -o rss,vsz,command | awk \'NR>1 {$1=int($1/1024)\"M\"; $2=int($2/1024)\"M\";}{ print ;}\' | grep \"Enterprise Client Connector\""
+        command = "ps x -o rss,vsz,command | awk \'NR>1 {$1=int($1/1024)\"M\"; $2=int($2/1024)\"M\";}{ print ;}\' | grep \"<process_name>\""
         output = subprocess.check_output(command, shell=True).decode(sys.stdout.encoding).strip()
         memory = output.split("M")[0]
         now = datetime.now()
